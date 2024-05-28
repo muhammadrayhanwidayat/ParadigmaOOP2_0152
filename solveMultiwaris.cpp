fc4ccd599b21277dd3393cbaba26e5044f1171a1
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-28 11:11:02 +0700

    membuat class orang dan pekerja

---
+++ solveMultiwaris.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public:
+    int umur;
+
+    orang(int pUmur):
+    umur(pUmur)
+    {
+        cout<<"orang dibuat dengan umur"<<umur<<"\n"<<endl;
+    }
+};
+
+class pekerja :virtual public orang{
+    
+}
+
+
+int main(){
+
+}
