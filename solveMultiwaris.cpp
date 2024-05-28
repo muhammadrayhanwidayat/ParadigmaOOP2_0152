044b544135725e3acf4941d8d4c18153a3197b84
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-28 11:44:39 +0700

    pembaruan

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
 using namespace std;
 
-class orang {
-    public:
-    int umur;
 
-    orang(int pUmur):
-    umur(pUmur)
-    {
-        cout<<"orang dibuat dengan umur"<<umur<<"\n"<<endl;
-    }
+class Orang {
+public:
+	int umur;
+
+	Orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
 };
 
-class pekerja :virtual public orang{
-    public:
-
-    pekerja(int pUmur):
-    orang(pUmur){
-        cout<<"pekerja berhasil dibuat\n"<<endl;
-    }
+class Pekerja :  public Orang {
+public:
+	Pekerja(int pUmur) :
+		Orang(pUmur) {
+		cout << "pekerja dibuat\n" << endl;
+	}
 };
 
-class pelajar :virtual public orang{
-    public:
-
-    pelajar(int pUmur):
-    orang(pUmur){
-        cout<<"pelajar berhasil dibuat\n"<<endl;
-    }
+class Pelajar :  public Orang {
+public:
+	Pelajar(int pUmur) :
+		Orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
 };
 
-class budi : public pekerja,public pelajar{
-    public:
-    budi(int pUmur):
-        pekerja(pUmur),
-        pelajar(pUmur),
-        orang(pUmur)//hal ini dapat dilakukan jika menggunakan virtual
-    {
-        cout<<"Budi dibuat\n"<<endl;
-    }
+class Budi : public Pekerja, public Pelajar {
+public:
+
+	Budi(int pUmur) :
+		Pekerja(pUmur),
+		Pelajar(pUmur)		//hal ini dapat dilakukan jika menggunakan virtual
+	{
+		cout << "budi dibuat\n" << endl;
+	}
 };
 
+int main() {
+	Budi a(12);
 
-int main(){
-    budi a(12);
-
-    return 0;
+	return 0;
 }
