8dc4b3df24f1aab96342082cc7fc735a758366da
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-28 11:23:39 +0700

    membuat perbaikan

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -13,10 +13,37 @@ class orang {
 };
 
 class pekerja :virtual public orang{
-    
-}
+    public:
+
+    pekerja(int pUmur):
+    orang(pUmur){
+        cout<<"pekerja berhasil dibuat\n"<<endl;
+    }
+};
+
+class pelajar :virtual public orang{
+    public:
+
+    pelajar(int pUmur):
+    orang(pUmur){
+        cout<<"pelajar berhasil dibuat\n"<<endl;
+    }
+};
+
+class budi : public pekerja,public pelajar{
+    public:
+    budi(int pUmur):
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur)//hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout<<"Budi dibuat\n"<<endl;
+    }
+};
 
 
 int main(){
+    budi a(12);
 
+    return 0;
 }
