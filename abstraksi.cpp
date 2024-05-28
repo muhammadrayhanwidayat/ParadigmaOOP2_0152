a5b366f73acfb3439a1ed5f4e2279e27307572a9
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-28 11:32:34 +0700

    pembuatan method dan pemanggilan di main

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,6 +4,27 @@ using namespace std;
 class Abstraksiklas {
 private: string x, y;
 
-int main(){
+public:
     
+    //method untuk mengisi nilai
+    //priavte member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+
+int main(){
+    Abstraksiklas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+
 }
